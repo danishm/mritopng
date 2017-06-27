19d685ef3581a42ff1f7d6ae75f58d6094e22208
Danish Mujeeb <danish.mujeeb@gmail.com>
2017-06-27 00:30:53 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='mritopng',
-    version='0.1',
+    version='2',
     description='Easily convert MRI filed based on the DICOM format to a PNG image',
     long_description=readme(),
     classifiers=[
