5e2fa089886536fa8303992d8d5863682d0727d1
Danish Mujeeb <danish.mujeeb@gmail.com>
2018-08-25 23:58:16 -0400

    add argument to apply auto contrast

--- mritopng/__main__.py
+++ mritopng/__main__.py
@@ -7,16 +7,18 @@ from . import convert_folder, convert_file
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Convert a dicom MRI file to png")
-    parser.add_argument('-f', action='store_true')
+    parser = argparse.ArgumentParser(description="Convert a dicom MRI file to png. To conver a whole folder recursivly, use the -f option")
+    parser.add_argument('-f', '--folder', action='store_true', help='Convert a whole folder instead of a single file')
+    parser.add_argument('-c', '--auto-contrast', help='Apply contrast after converting default image', action="store_true")
     parser.add_argument('dicom_path', help='Full path to the mri file')
     parser.add_argument('png_path', help='Full path to the generated png file')
 
     args = parser.parse_args()
-    if args.f:
-        convert_folder(args.dicom_path, args.png_path)
+    print('Arguments: %s', args)
+    if args.folder:
+        convert_folder(args.dicom_path, args.png_path, args.auto_contrast)
     else:
-        convert_file(args.dicom_path, args.png_path)
+        convert_file(args.dicom_path, args.png_path, args.auto_contrast)
 
 if __name__ == '__main__':
     main()
