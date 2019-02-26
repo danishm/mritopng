feabeda502ff12a52aeb533509885a90cc687362
kshitij12345 <kshitijkalambarkar@gmail.com>
2019-02-26 15:22:26 +0530

    use numpy functions
     using numpy functions will allow to reduce the code
     and lead to easier maintenance, without reinventing
     the wheel.

--- mritopng/contrast.py
+++ mritopng/contrast.py
@@ -1,36 +1,20 @@
 import numpy as np
 from .models import GrayscaleImage
 
-def histogram(image):
-    
-    hist = dict()
-
-    # Initialize dict
-    for shade in range(0, 256):
-        hist[shade] = 0
-    
-    for index, val in np.ndenumerate(image.image):
-        hist[val] += 1
-      
-    return hist
-
-
 def shade_at_percentile(hist, percentile):
-
-    n = sum(hist.values())
-    cumulative_sum = 0.0
-    for shade in range(0, 256):
-        cumulative_sum += hist[shade]
-        if cumulative_sum/n >= percentile:
-            return shade
+    """ Assumes the argument percentile,
+        to be less 1.
+    """
+    n = np.sum(hist)
+    cumulative_sum = np.cumsum(hist)
     
-    return None
+    return np.argmax(cumulative_sum/n >= percentile)
 
 def auto_contrast(image):
     """ Apply auto contrast to an image using
         https://stackoverflow.com/questions/9744255/instagram-lux-effect/9761841#9761841
     """
-    hist = histogram(image)
+    hist, _ = np.histogram(image.image.ravel(), bins=np.arange(0, 256))
     p5 = shade_at_percentile(hist, .01)
     p95 = shade_at_percentile(hist, .99)
     a = 255.0/(p95 + p5)
